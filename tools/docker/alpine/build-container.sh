9286ca814a342e358e2ca05e4089e3bc35a03864
Fabian <copy@copy.sh>
2024-04-27 20:49:40 +0900

    improve alpine build process
    fix for podman
    add nodejs with hello world as example package
    add missing modules=virtio_pci to fix boot process

--- tools/docker/alpine/build-container.sh
+++ tools/docker/alpine/build-container.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -veu
+set -euo pipefail
 
 IMAGES="$(dirname "$0")"/../../../images
 OUT_ROOTFS_TAR="$IMAGES"/alpine-rootfs.tar
@@ -16,7 +16,7 @@ docker create --platform linux/386 -t -i --name "$CONTAINER_NAME" "$IMAGE_NAME"
 docker export "$CONTAINER_NAME" -o "$OUT_ROOTFS_TAR"
 
 # https://github.com/iximiuz/docker-to-linux/issues/19#issuecomment-1242809707
-tar -f "$OUT_ROOTFS_TAR" --delete ".dockerenv"
+tar -f "$OUT_ROOTFS_TAR" --delete ".dockerenv" || true
 
 "$(dirname "$0")"/../../../tools/fs2json.py --out "$OUT_FSJSON" "$OUT_ROOTFS_TAR"
 
@@ -25,3 +25,5 @@ mkdir -p "$OUT_ROOTFS_FLAT"
 "$(dirname "$0")"/../../../tools/copy-to-sha256.py "$OUT_ROOTFS_TAR" "$OUT_ROOTFS_FLAT"
 
 echo "$OUT_ROOTFS_TAR", "$OUT_ROOTFS_FLAT" and "$OUT_FSJSON" created.
+
+# TODO: auto-build state, add examples/alpine.html
